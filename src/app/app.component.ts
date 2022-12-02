c772bba9125a52fe235c0d6a166ab3e00797d20e
rigomatuja <rodrigo.obalat@gmail.com>
2022-12-02 10:27:20 +0100

    redistributed some logic and html elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,17 +39,19 @@ export class AppComponent {
     readonly coffeeNames = CoffeeNames;
     readonly coffeeTypes = CoffeeTypes;
     readonly coffeePrices = CoffeePrices;
-    readonly preparationTime = 3;
-    readonly defaultMessage = 'Seleccione un café...'
+    readonly preparationTime = 2;
+    readonly defaultMessage = 'Seleccione un café...';
+    readonly itemsPerPage = 5;
 
-    history: MachineHistory[] = [];
-    balance: number = 0;
     isMakingCoffee: boolean = false;
-    progress: number = 0;
-    interval: number = 0;
-    machineDisplayMessage: string = this.defaultMessage;
     displayChart: boolean = false;
     displayRevenue: boolean = false;
+    balance: number = 0;
+    progress: number = 0;
+    intervalId: number = 0;
+    machineDisplayMessage: string = this.defaultMessage;
+    history: MachineHistory[] = [];
+    revenueData: number[] = [];
     chartData = {
         labels: [this.coffeeNames.get(CoffeeTypes.coffee), this.coffeeNames.get(CoffeeTypes.macchiato), this.coffeeNames.get(CoffeeTypes.withMilk), this.coffeeNames.get(CoffeeTypes.cappuccino)],
         datasets: [
@@ -70,47 +72,20 @@ export class AppComponent {
             }
         ]
     };
-    revenueData: number[] = [];
-
 
     constructor(
         private messageService: MessageService,
         private confirmationService: ConfirmationService
     ) {}
 
-
-    addBalance(value: number){
-        this.balance = this.balance + value;
-        this.messageService.add({
-            severity: 'success',
-            summary: 'Añadir Saldo',
-            detail: `Se ha añadido ${value}€ de saldo`
-        });
-    }
-
     makeCoffee(coffeeType: CoffeeTypes){
-        if(this.isMakingCoffee) return;
-
-        const enoughBalance = this.balance && this.balance >= (this.coffeePrices.get(coffeeType) ?? 0)
-        if(!enoughBalance){
-            this.machineDisplayMessage = 'Saldo Insuficiente!';
-            return;
-        }
-        const history = this.history;
+        if(!this.canMakeCoffee(coffeeType)) return;
 
         this.isMakingCoffee = true;
         this.balance = parseFloat((this.balance - (this.coffeePrices.get(coffeeType) ?? 0)).toFixed(2));
-        history.push({
-            coffeeType: coffeeType,
-            coffeeName: this.coffeeNames.get(coffeeType) ?? '',
-            price: this.coffeePrices.get(coffeeType) ?? 0,
-            timestamp: Date.now()
-        });
-        this.history = [...history];
-
         this.machineDisplayMessage = `Preparando Café ${this.coffeeNames.get(coffeeType)}...`;
         let time = 0;
-        this.interval = setInterval(() => {
+        this.intervalId = setInterval(() => {
             time = time +1;
             this.progress = Math.floor(time * 100 / this.preparationTime);
             if(this.progress === 100){
@@ -119,38 +94,22 @@ export class AppComponent {
 
                 setTimeout(() => {
                     this.isMakingCoffee = false;
+                    this.progress = 0;
                     this.machineDisplayMessage = this.defaultMessage;
                 }, 2000);
             }
         }, 1000);
+
+        this.updateCoffeeSellsHistory(coffeeType);
     }
 
-    showSellsDataModal(){
-        this.calculateChartData();
-
-        this.displayChart = true;
-    }
-
-    showSellsRevenue(){
-        this.calculateChartData();
-
-        this.displayRevenue = true;
-    }
-
-    calculateChartData(){
-        const coffeeSells = this.history.filter((coffee) => coffee.coffeeType === this.coffeeTypes.coffee).length;
-        const macchiatoSells = this.history.filter((coffee) => coffee.coffeeType === this.coffeeTypes.macchiato).length;
-        const withMilkSells = this.history.filter((coffee) => coffee.coffeeType === this.coffeeTypes.withMilk).length;
-        const cappuccinoSells = this.history.filter((coffee) => coffee.coffeeType === this.coffeeTypes.cappuccino).length;
-
-        this.chartData.datasets[0].data = [coffeeSells, macchiatoSells, withMilkSells, cappuccinoSells];
-
-        this.revenueData = [
-            coffeeSells * (this.coffeePrices.get(this.coffeeTypes.coffee) ?? 0),
-            macchiatoSells * (this.coffeePrices.get(this.coffeeTypes.macchiato) ?? 0),
-            withMilkSells * (this.coffeePrices.get(this.coffeeTypes.withMilk) ?? 0),
-            cappuccinoSells * (this.coffeePrices.get(this.coffeeTypes.cappuccino) ?? 0),
-        ];
+    addBalance(value: number){
+        this.balance = this.balance + value;
+        this.messageService.add({
+            severity: 'success',
+            summary: 'Añadir Saldo',
+            detail: `Se ha añadido ${value}€ de saldo`
+        });
     }
 
     resetBalance(){
@@ -162,7 +121,15 @@ export class AppComponent {
         });
     }
 
-    confirm(event: Event) {
+    showSellsDataModal(){
+        this.displayChart = true;
+    }
+
+    showSellsRevenue(){
+        this.displayRevenue = true;
+    }
+
+    confirmBalanceReset(event: Event) {
         this.confirmationService.confirm({
             //@ts-ignore
             target: event.target,
@@ -179,7 +146,49 @@ export class AppComponent {
         });
     }
 
+    private updateCoffeeSellsHistory(coffeeType: CoffeeTypes){
+        const history = this.history;
+
+        history.push({
+            coffeeType: coffeeType,
+            coffeeName: this.coffeeNames.get(coffeeType) ?? '',
+            price: this.coffeePrices.get(coffeeType) ?? 0,
+            timestamp: Date.now()
+        });
+
+        this.history = [...history];
+        this.calculateChartData();
+    }
+
+    private canMakeCoffee(coffeeType: CoffeeTypes){
+        if(this.isMakingCoffee) return false;
+
+        const enoughBalance = this.balance && this.balance >= (this.coffeePrices.get(coffeeType) ?? 0)
+        if(!enoughBalance){
+            this.machineDisplayMessage = 'Saldo Insuficiente!';
+            return false;
+        }
+
+        return true;
+    }
+
+    private calculateChartData(){
+        const coffeeSells = this.history.filter((coffee) => coffee.coffeeType === this.coffeeTypes.coffee).length;
+        const macchiatoSells = this.history.filter((coffee) => coffee.coffeeType === this.coffeeTypes.macchiato).length;
+        const withMilkSells = this.history.filter((coffee) => coffee.coffeeType === this.coffeeTypes.withMilk).length;
+        const cappuccinoSells = this.history.filter((coffee) => coffee.coffeeType === this.coffeeTypes.cappuccino).length;
+
+        this.chartData.datasets[0].data = [coffeeSells, macchiatoSells, withMilkSells, cappuccinoSells];
+
+        this.revenueData = [
+            coffeeSells * (this.coffeePrices.get(this.coffeeTypes.coffee) ?? 0),
+            macchiatoSells * (this.coffeePrices.get(this.coffeeTypes.macchiato) ?? 0),
+            withMilkSells * (this.coffeePrices.get(this.coffeeTypes.withMilk) ?? 0),
+            cappuccinoSells * (this.coffeePrices.get(this.coffeeTypes.cappuccino) ?? 0),
+        ];
+    }
+
     private clearInterval(){
-        clearInterval(this.interval);
+        clearInterval(this.intervalId);
     }
 }
